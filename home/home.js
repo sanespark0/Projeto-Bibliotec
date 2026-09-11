1ad7c0be302103ea4e0ef6f6dd1adb27b8ff3b1d
JOSANE OLIVEIRA ROCHA <12017688533@info.local>
2026-09-11 09:35:55 -0300

    :bug:subindo novo codigo

---
+++ home/home.js
@@ -0,0 +1,20 @@
+
+const menu = document.getElementById("menu"); /* pegando o elemento pelo ID */
+
+// estamos pegando o elemento de forma genérica - pode ser classe (precisa trazer o ponto) ou pode ser ID (precisa trazer a hashtag).
+const navMenu = document.querySelector(".navegacao"); 
+
+
+menu.addEventListener("click", function() {
+
+    if(navMenu.className == "navegacao") {
+        navMenu.className = "navegacao ativo";
+    }
+    else {
+        navMenu.className = "navegacao";
+    }
+})
+
+
+// opção com arrow function -> função lambda
+// menu.addEventListener("click", () => {})
--- login/login.js
+++ login/login.js
@@ -1,36 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const campoSenha = document.getElementById("senha");
+    const btnSenha = document.getElementById("mostrar-senha");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const campoSenha= document.getElementById("senha");
-const btnSenha= document.querySelector("#mostrar-senha");
-
-console.log(campoSenha.type);
-
-btnSenha.addEventListener("click", function(){
-//
- //   if(campoSenha.type == "passoword") {
-  //      campoSenha.type = "text";
-  //  }
- //   else {
-   //     campoSenha.type = "passowrd";
-   // }
-   campoSenha.type = campoSenha.type == "pasoword" ? "text" : "passoword"
-
-})
+    if (campoSenha && btnSenha) {
+        btnSenha.addEventListener("click", () => {
+            campoSenha.type = campoSenha.type === "password" ? "text" : "password";
+        });
+    }
+});
